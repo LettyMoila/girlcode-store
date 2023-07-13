3fbdbd5b3b75916097b1abedcf93bbd6e29e106a
LettyMoila <moilankibelo@gmail.com>
2023-07-13 04:34:05 +0200

    polished deals section

--- src/components/DealsSection.jsx
+++ src/components/DealsSection.jsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from 'react';
 import ProductItem from './ProductItem'
 
-const DealsSection = ({prod_info}) =>{
+const DealsSection = () =>{
     const [prodList, setProdList] = useState([])
 
     useEffect(()=> {
-        fetch(`$(import.meta.env.VITE_API_URL)/products`)
+        fetch(`${import.meta.env.VITE_API_URL}/products`)
             .then(resp >= resp.json())
             .then(resp => {                                                                                                                                
                 setProdList(resp.products)
                 console.log(resp)
             })
             .catch(error => {
-                console.log(SyntaxError)
+                console.log(error)
             })
     }, [])
 
     return(
-        <section className="pt-26">
+        <section className="pt-20">
             <div className="container mx-auto">
-                <div className="mt-9">
+                <div className="mb-9">
                 <h1 className="text-2xl font-extrabold">Todays Best Deals For You!</h1>
                 </div>
 
