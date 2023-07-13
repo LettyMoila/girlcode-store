6f5e43758974c27bb05a72cd6c38b3a0b279d8e9
LettyMoila <moilankibelo@gmail.com>
2023-07-13 05:19:45 +0200

    final touch on navbar component

--- src/components/Nav-Bar.jsx
+++ src/components/Nav-Bar.jsx
@@ -1,12 +1,14 @@
+import {Link} from "react-router-dom"
 import logo from "../assets/girlcode_img.jpg"
 
 const Navbar = () =>{
-                   return(
-        <div className="nav-bar py-6">
+
+    return(
+        <div className="nav-bar py-3">
             <div className="container ma-auto flex justify-between items-center">
-                <a href="#" className="a-46 block">
+                <Link href="#" className="a-46 block">
                     <img src={logo} alt="girlcode store logo"/>
-                </a>
+                </Link>
 
                 <ul className="flex gap-6">
                     <li>
@@ -24,14 +26,14 @@ const Navbar = () =>{
                 </ul>
 
                 <div className="flex gap-6 items-center">
-                    <form  className="bg-aLate-300 w-fit py-1.5 px-5 rounded-pxl">
+                    <form  className="bg-slate-200 w-fit py-1.5 px-5 rounded-3xl">
                         <input type="text" name="search" placeholder="Search Products" className="outline-0 bg-transparent"/>
                         <button>
                             <span className="material-symbol-outlined align-middle">search</span>
                         </button>
                     </form>
                     <a href="" className="font-bold"><span className="material-symbols-outlined align-middle"></span>Account</a>
-                    <a href="" className="font-bold"><span className="material-symbols-outlined align-middle"></span>Cart</a>
+                    <Link to="/cart" className="font-bold"><span className="material-symbols-outlined align-middle"></span>Cart</a>
                 </div>
             </div>
         </div>
