999e3791b7479a2ba93563ad30dfe71874b75813
LettyMoila <moilankibelo@gmail.com>
2023-07-13 12:34:27 +0200

    furnished category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import MainLayout from "../layouts/MainLayout"
+import ProductItem from "../components/ProductItem"
 
 const Category = () =>{
     let {catSlug} = useParams()
@@ -8,7 +9,7 @@ const Category = () =>{
     const [prodList, setProdList] = useState([])
 
     useEffect(()=> {
-        fetch(`$(import.meta.env.VITE_API_URL)/category/${catSlug}`)
+        fetch(`${import.meta.env.VITE_API_URL}/category/${catSlug}`)
             .then(resp >= resp.json())
             .then(resp => {                                                                                                                                
                 setCatInfo(resp)
@@ -17,14 +18,14 @@ const Category = () =>{
                 console.log(error)
             })
 
-            fetch(`$(import.meta.env.VITE_API_URL)/category/${catSlug}/products`)
+            fetch(`${import.meta.env.VITE_API_URL}/category/${catSlug}/products`)
             .then(resp >= resp.json())
             .then(resp => {                                                                                                                                
                 setProdList(resp.products)
                 console.log(resp)
             })
             .catch(error => {
-                console.log(SyntaxError)
+                console.log(error)
             })
     }, [catSlug])
 
@@ -34,32 +35,30 @@ const Category = () =>{
             <section>
                 <div className="mx-auto bg-page-header bg-no-repeat bg-cover py-20 px-16">
                     <div className="w-3/5">
-                        <h1 className="text-6xl font-bold leading-tight text-girlcode-pink">{catInfo.desc}</h1>
-                        <a href="#" className="bg-girlcode-pink text-white py-2 px-4 font-bold text-lg rounded-3xl mt-9 block w-fit">Buy now</a>
+                        <h1 className="text-6xl font-extrabold leading-tight text-girlcode-pink">{catInfo.desc}</h1>
+                        <a href="#" className="btn bg-girlcode-pink text-white text-lg mt-9">Buy now</a>
                     </div>
-                    
                 </div>
-
             </section>
             <section className="pt-20">
                 <div className="mx-auto">
-                    <div className="btn bg-gray-300">{catInfo.name} Type</div>
+                    <div className="btn bg-gray-300">{catInfo.name} Type<span className="material-symbols-outlined align-middle">expand_more</span></div>
                 </div>
             </section>
             <section className="py-20">
                 <div className="container mx-auto">
-                    <div className="mt-9">
-                        <h1 className="text-2xl font-extrabold">{catInfo.name} Deals For You!</h1>
+                    <div className="mb-9">
+                        <h2 className="text-2xl font-extrabold">{catInfo.name} Deals For You!</h2>
                     </div>
 
-                    <div className="grid gid-cols-3 gap-8">
+                    <div className="grid grid-cols-4 gap-8">
                     {
                         prodList.map(prod =>(
                             <ProductItem key={prod.id} prod_info={prod}/>
                         ))
                     }
                 
-                    /</div>
+                    </div>
                 </div>
             </section>
         </MainLayout>
